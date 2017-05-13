0061b6eac3f44c153a71e4cdaf97c384ba7c895c
Dan Stocker <dan@kwaia.com>
2017-05-13 08:31:48 +0100

    adds skeleton for module giant-data

---
+++ modules/data/globals.js
@@ -0,0 +1,12 @@
+/* global require */
+"use strict";
+
+var $assert = require('giant-assert'),
+    $oop = require('giant-oop'),
+    $utils = require('giant-utils'),
+    hOP = Object.prototype.hasOwnProperty,
+    slice = Array.prototype.slice;
+
+/**
+ * @namespace $data
+ */
