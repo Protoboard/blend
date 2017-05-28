bf89e344d78a84e460a1b3c8c8f54aa6d5f6ea26
Dan Stocker <dan@kwaia.com>
2017-05-28 08:44:38 +0100

    adds tests for StringCollection

---
+++ modules/data/key-value/StringCollection.test.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var $assert = window['giant-assert'],
+    $data = window['giant-data'];
+
+describe("$data", function () {
+    var data,
+        result;
+
+    describe("DataContainer", function () {
+        describe("toStringCollection()", function () {
+            var container = $data.DataContainer.create([1, 2, 3]);
+
+            beforeEach(function () {
+                result = container.toStringCollection();
+            });
+
+            it("should return a StringCollection instance", function () {
+                expect($data.StringCollection.isIncludedBy(result))
+                    .toBeTruthy();
+            });
+
+            it("should set data buffer", function () {
+                expect(result._data).toBe(container._data);
+            });
+        });
+    });
+});
+
+describe("Array", function () {
+    var result;
+
+    describe("toStringCollection()", function () {
+        var array = ['a', 'b', 'c'];
+
+        beforeEach(function () {
+            result = array.toStringCollection();
+        });
+
+        it("should return a StringCollection instance", function () {
+            expect($data.StringCollection.isIncludedBy(result)).toBeTruthy();
+        });
+
+        it("should set data buffer", function () {
+            expect(result._data).toBe(array);
+        });
+    });
+});
