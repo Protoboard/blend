b9a51e12539e0374017fee84408775b0a0a5c092
Dan Stocker <dan@kwaia.com>
2017-05-30 08:35:50 +0100

    re-adds StringSet

---
+++ modules/data/set/StringSet.js
@@ -0,0 +1,185 @@
+/* globals $assert, $data, $oop, $utils, hOP, slice */
+"use strict";
+
+/**
+ * @function $data.StringSet.create
+ * @param {object|Array} [data]
+ * @returns {$data.StringSet}
+ */
+
+/**
+ * @class $data.StringSet
+ * @extends $data.DataContainer
+ * @mixes $data.ItemContainer
+ */
+$data.StringSet = $oop.getClass('$data.StringSet')
+    .extend($oop.getClass('$data.DataContainer'))
+    .include($oop.getClass('$data.ItemContainer'))
+    .define(/** @lends $data.StringSet# */{
+        /**
+         * @param {string} item
+         * @returns {$data.StringSet}
+         */
+        setItem: function (item) {
+            var data = this._data;
+            if (!hOP.call(data, item)) {
+                data[item] = 1;
+
+                if (this._itemCount !== undefined) {
+                    this._itemCount++;
+                }
+            }
+            return this;
+        },
+
+        /**
+         * @param {string} item
+         * @returns {$data.StringSet}
+         */
+        deleteItem: function (item) {
+            var data = this._data;
+            if (hOP.call(data, item)) {
+                delete data[item];
+
+                if (this._itemCount !== undefined) {
+                    this._itemCount--;
+                }
+            }
+            return this;
+        },
+
+        /**
+         * @param {string} item
+         * @returns {boolean}
+         */
+        hasItem: function (item) {
+            return hOP.call(this._data, item);
+        },
+
+        /**
+         * @param {function} callback
+         * @param {object} [context]
+         * @returns {$data.StringSet}
+         */
+        forEachItem: function (callback, context) {
+            var items = Object.keys(this._data),
+                itemCount = items.length,
+                i, item;
+
+            for (i = 0; i < itemCount; i++) {
+                item = items[i];
+                if (callback.call(context || this, item) === false) {
+                    break;
+                }
+            }
+
+            return this;
+        },
+
+        /**
+         * Intersects current set with specified set and returns the result
+         * as a new set.
+         * @param {$data.StringSet} set Set to intersect current set with
+         * @returns {$data.StringSet} Intersection
+         */
+        intersectWith: function (set) {
+            var result = $oop.getClass(this.__classId).create();
+            this.forEachItem(function (item) {
+                if (set.hasItem(item)) {
+                    result.setItem(item);
+                }
+            });
+            return result;
+        },
+
+        /**
+         * Unites current set with specified set and returns the result
+         * as a new set.
+         * @param {$data.StringSet} set Set to unite current set with
+         * @returns {$data.StringSet} Union
+         */
+        uniteWith: function (set) {
+            var result = $oop.getClass(this.__classId).create();
+            this.forEachItem(function (item) {
+                result.setItem(item);
+            });
+            set.forEachItem(function (item) {
+                result.setItem(item);
+            });
+            return result;
+        },
+
+        /**
+         * Subtracts specified set from current set and returns difference
+         * as a new set.
+         * @param {$data.StringSet} set Set to subtract from current set
+         * @returns {$data.StringSet} Difference
+         */
+        subtract: function (set) {
+            var result = $oop.getClass(this.__classId).create();
+            this.forEachItem(function (item) {
+                if (!set.hasItem(item)) {
+                    result.setItem(item);
+                }
+            });
+            return result;
+        },
+
+        /**
+         * Subtracts current set from specified set and returns difference
+         * as a new set.
+         * @param {$data.StringSet} set Set to subtract current set from
+         * @returns {$data.StringSet} Difference
+         */
+        subtractFrom: function (set) {
+            var that = this,
+                result = $oop.getClass(this.__classId).create();
+            set.forEachItem(function (item) {
+                if (!that.hasItem(item)) {
+                    result.setItem(item);
+                }
+            });
+            return result;
+        },
+
+        /**
+         * Takes symmetric difference between current set and specified set
+         * and returns result as a new set.
+         * @param {$data.StringSet} set Set to take symmetric difference with
+         * @returns {$data.StringSet} Difference
+         */
+        takeDifferenceWith: function (set) {
+            var that = this,
+                result = $oop.getClass(this.__classId).create();
+            this.forEachItem(function (item) {
+                if (!set.hasItem(item)) {
+                    result.setItem(item);
+                }
+            });
+            set.forEachItem(function (item) {
+                if (!that.hasItem(item)) {
+                    result.setItem(item);
+                }
+            });
+            return result;
+        }
+    });
+
+$oop.getClass('$data.DataContainer')
+    .delegate(/** @lends $data.DataContainer# */{
+        /**
+         * @returns {$data.StringSet}
+         */
+        toStringSet: function () {
+            return $data.StringSet.create(this._data);
+        }
+    });
+
+$oop.copyProperties(Array.prototype, /** @lends external:Array# */{
+    /**
+     * @returns {$data.StringSet}
+     */
+    toStringSet: function () {
+        return $data.StringSet.create(this);
+    }
+});
---
+++ modules/data/set/StringSet.test.js
@@ -0,0 +1,270 @@
+"use strict";
+
+var $assert = window['giant-assert'],
+    $oop = window['giant-oop'],
+    $utils = window['giant-utils'],
+    $data = window['giant-data'];
+
+describe("$data", function () {
+    var data,
+        StringSet,
+        set, set2,
+        result;
+
+    describe("StringSet", function () {
+        beforeEach(function () {
+            data = {
+                foo: 1,
+                bar: 1
+            };
+            StringSet = $oop.getClass("StringSet")
+                .extend($data.StringSet);
+            set = StringSet.create(data);
+            set2 = StringSet.create({
+                bar: 1,
+                baz: 1
+            });
+        });
+
+        describe("setItem()", function () {
+            beforeEach(function () {
+                set._itemCount = 2;
+                result = set.setItem('baz');
+            });
+
+            it("should return self", function () {
+                expect(result).toBe(set);
+            });
+
+            it("should add item", function () {
+                expect(result._data).toEqual({
+                    foo: 1,
+                    bar: 1,
+                    baz: 1
+                });
+            });
+
+            it("should increment _itemCount", function () {
+                expect(result._itemCount).toBe(3);
+            });
+
+            describe("when re-adding existing item", function () {
+                beforeEach(function () {
+                    set.setItem('foo');
+                });
+
+                it("should not increment _itemCount", function () {
+                    expect(result._itemCount).toBe(3);
+                });
+            });
+        });
+
+        describe("deleteItem()", function () {
+            beforeEach(function () {
+                set._itemCount = 2;
+                result = set.deleteItem('bar');
+            });
+
+            it("should return self", function () {
+                expect(result).toBe(set);
+            });
+
+            it("should delete item", function () {
+                expect(result._data).toEqual({
+                    foo: 1
+                });
+            });
+
+            it("should decrement _itemCount", function () {
+                expect(result._itemCount).toBe(1);
+            });
+
+            describe("when deleting absent item", function () {
+                beforeEach(function () {
+                    set.deleteItem('bar');
+                });
+
+                it("should not decrement _itemCount", function () {
+                    expect(result._itemCount).toBe(1);
+                });
+            });
+        });
+
+        describe("hasItem()", function () {
+            describe("for existing item", function () {
+                it("should return true", function () {
+                    expect(set.hasItem('foo')).toBeTruthy();
+                });
+            });
+            describe("for absent item", function () {
+                it("should return false", function () {
+                    expect(set.hasItem('baz')).toBeFalsy();
+                });
+            });
+        });
+
+        describe("forEachItem()", function () {
+            var callback;
+
+            beforeEach(function () {
+                callback = jasmine.createSpy();
+                result = set.forEachItem(callback);
+            });
+
+            it("should return self", function () {
+                expect(result).toBe(set);
+            });
+
+            it("should pass items to callback", function () {
+                expect(callback.calls.allArgs()).toEqual([
+                    ['foo'],
+                    ['bar']
+                ]);
+            });
+
+            describe("when interrupted", function () {
+                beforeEach(function () {
+                    callback = jasmine.createSpy().and.returnValue(false);
+                    set.forEachItem(callback);
+                });
+
+                it("should stop at interruption", function () {
+                    expect(callback).toHaveBeenCalledTimes(1);
+                });
+            });
+        });
+
+        describe("intersectWith()", function () {
+            beforeEach(function () {
+                result = set.intersectWith(set2);
+            });
+
+            it("should return new StringSet instance", function () {
+                expect(StringSet.isIncludedBy(result));
+                expect(result).not.toBe(set);
+                expect(result).not.toBe(set2);
+            });
+
+            it("should create intersection with values from original set",
+                function () {
+                    expect(result._data).toEqual({
+                        bar: 1
+                    });
+                });
+        });
+
+        describe("uniteWith()", function () {
+            beforeEach(function () {
+                result = set.uniteWith(set2);
+            });
+
+            it("should return new StringSet instance", function () {
+                expect(StringSet.isIncludedBy(result));
+                expect(result).not.toBe(set);
+                expect(result).not.toBe(set2);
+            });
+
+            it("should create union with original values taking precedence",
+                function () {
+                    expect(result._data).toEqual({
+                        foo: 1,
+                        bar: 1,
+                        baz: 1
+                    });
+                });
+        });
+
+        describe("subtract()", function () {
+            beforeEach(function () {
+                result = set.subtract(set2);
+            });
+
+            it("should return new StringSet instance", function () {
+                expect(StringSet.isIncludedBy(result));
+                expect(result).not.toBe(set);
+                expect(result).not.toBe(set2);
+            });
+
+            it("should subtract set from original", function () {
+                expect(result._data).toEqual({
+                    foo: 1
+                });
+            });
+        });
+
+        describe("subtractFrom()", function () {
+            beforeEach(function () {
+                result = set.subtractFrom(set2);
+            });
+
+            it("should return new StringSet instance", function () {
+                expect(StringSet.isIncludedBy(result));
+                expect(result).not.toBe(set);
+                expect(result).not.toBe(set2);
+            });
+
+            it("should subtract set from original", function () {
+                expect(result._data).toEqual({
+                    baz: 1
+                });
+            });
+        });
+
+        describe("takeDifferenceWith()", function () {
+            beforeEach(function () {
+                result = set.takeDifferenceWith(set2);
+            });
+
+            it("should return new StringSet instance", function () {
+                expect(StringSet.isIncludedBy(result));
+                expect(result).not.toBe(set);
+                expect(result).not.toBe(set2);
+            });
+
+            it("should return symmetric difference", function () {
+                expect(result._data).toEqual({
+                    foo: 1,
+                    baz: 1
+                });
+            });
+        });
+    });
+
+    describe("DataContainer", function () {
+        describe("toStringSet()", function () {
+            var buffer = $data.DataContainer.create([1, 2, 3]);
+
+            beforeEach(function () {
+                result = buffer.toStringSet();
+            });
+
+            it("should return a StringSet instance", function () {
+                expect($data.StringSet.isIncludedBy(result)).toBeTruthy();
+            });
+
+            it("should set data set", function () {
+                expect(result._data).toBe(buffer._data);
+            });
+        });
+    });
+});
+
+describe("Array", function () {
+    var result;
+
+    describe("toStringSet()", function () {
+        var array = [1, 2, 3];
+
+        beforeEach(function () {
+            result = array.toStringSet();
+        });
+
+        it("should return a StringSet instance", function () {
+            expect($data.StringSet.isIncludedBy(result)).toBeTruthy();
+        });
+
+        it("should set data set", function () {
+            expect(result._data).toBe(array);
+        });
+    });
+});
