61344c17a451b6494873671a43b14461a546bf7b
Dan Stocker <dan@kwaia.com>
2017-06-01 18:07:37 +0100

    fixes Cloneable passing original ctr arguments when cloning instance

--- modules/utils/class/Cloneable.js
+++ modules/utils/class/Cloneable.js
@@ -1,17 +1,27 @@
 "use strict";
 
 /**
- * Cloning creates a new instance of identical class and state.
+ * Creates a new instance of identical class and state.
  * @mixin $utils.Cloneable
  */
 $utils.Cloneable = $oop.getClass('$utils.Cloneable')
     .define(/** @lends $utils.Cloneable# */{
+        /** @ignore */
+        init: function () {
+            /**
+             * Constructor arguments.
+             * @type {Arguments}
+             * @private
+             */
+            this._ctrArguments = arguments;
+        },
+
         /**
          * Clones current instance.
          * @returns {$utils.Cloneable}
          */
         clone: function clone() {
             var Class = $oop.getClass(this.__classId);
-            return Class.create();
+            return Class.create.apply(Class, this._ctrArguments);
         }
     });
--- modules/utils/class/Cloneable.spec.js
+++ modules/utils/class/Cloneable.spec.js
@@ -16,18 +16,30 @@ describe("$utils", function () {
                         return clone.returned;
                     }
                 });
-            cloneable = Cloneable.create();
+            cloneable = Cloneable.create('foo');
+        });
+
+        describe("create()", function () {
+            it("should set _ctrArguments property", function () {
+                var args = Array.prototype.slice.call(cloneable._ctrArguments);
+                expect(args).toEqual(['foo']);
+            });
         });
 
         describe("clone()", function () {
             var clone;
 
             beforeEach(function () {
+                spyOn(Cloneable, 'create').and.callThrough();
                 clone = cloneable.clone();
             });
 
             it("should return new instance", function () {
                 expect(Cloneable.isPrototypeOf(clone)).toBeTruthy();
+            });
+
+            it("should pass ctr arguments to create()", function () {
+                expect(Cloneable.create).toHaveBeenCalledWith('foo');
             });
         });
     });
