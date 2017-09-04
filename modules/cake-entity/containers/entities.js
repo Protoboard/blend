70ff150dc0f3ea4670d7db0253112c21af226bb3
Dan Stocker <dan@kwaia.com>
2017-09-04 10:59:39 +0100

    Adds containers

---
+++ modules/cake-entity/containers/entities.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/**
+ * Stores entities.
+ * @type {$data.Tree}
+ */
+$entity.entities = $data.Tree.create();
---
+++ modules/cake-entity/containers/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/**
+ * Stores user-defined indexes.
+ * @type {$data.Tree}
+ */
+$entity.index = $data.Tree.create();
