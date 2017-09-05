bd591c5e52366070f7a3e4ee28f8a164b3568440
Dan Stocker <dan@kwaia.com>
2017-09-05 14:31:50 +0100

    Merges entity config container into entity container

--- modules/cake-entity/containers/entities.js
+++ modules/cake-entity/containers/entities.js
@@ -4,4 +4,39 @@
  * Stores entities.
  * @type {$data.Tree}
  */
-$entity.entities = $data.Tree.create();
+$entity.entities = $data.Tree.fromData({
+  document: {
+    /**
+     * Document-descriptor metadata documents.
+     */
+    __document: {
+      //user: {
+      //  documentType: 'user'
+      //}
+    },
+
+    /**
+     * Field-descriptor metadata documents.
+     */
+    __field: {
+      //'user/name': {
+      //  /** Field contains string */
+      //  fieldType: 'string'
+      //},
+      //'user/emails': {
+      //  /** Field contains collection */
+      //  fieldType: 'collection',
+      //  /** Items are strings */
+      //  itemType: 'string'
+      //},
+      //'user/friends': {
+      //  /** Field contains collection */
+      //  fieldType: 'collection',
+      //  /** Items are booleans */
+      //  itemType: 'boolean',
+      //  /** Item IDs are references */
+      //  itemIdType: 'reference'
+      //}
+    }
+  }
+});
