36deb87f4c96f7869f3911e91465f22c852de1a8
Dan Stocker <dan@kwaia.com>
2017-09-06 15:29:10 +0100

    Adds access methods to $entity.Entity

--- modules/cake-entity/entities/Entity.js
+++ modules/cake-entity/entities/Entity.js
@@ -31,5 +31,70 @@ $entity.Entity = $oop.getClass('$entity.Entity')
     return this.create({
       entityKey: entityKey
     });
+  },
+
+  /**
+   * Retrieves data node associated with the current entity.
+   * @returns {*}
+   */
+  getNode: function () {
+    var entityPath = this.entityKey.getEntityPath(),
+        node = $entity.entities.getNode(entityPath);
+
+    // todo Handling invalidated access
+
+    if (node === undefined) {
+      this.trigger($entity.EVENT_ENTITY_ABSENT);
+    }
+
+    return node;
+  },
+
+  /**
+   * Retrieves data node associated with the current entity, wrapped in a
+   * `DataContainer` instance.
+   * @returns {$data.DataContainer}
+   */
+  getNodeWrapped: function () {
+    return $data.DataContainer.create({data: this.getNode()});
+  },
+
+  /**
+   * Retrieves data node associated with the current entity, without
+   * triggering events.
+   * @returns {*}
+   */
+  getSilentNode: function () {
+    var entityPath = this.entityKey.getEntityPath();
+    return $entity.entities.getNode(entityPath);
+  },
+
+  /**
+   * Retrieves data node associated with the current entity, without
+   * triggering events, wrapped in a `DataContainer` instance.
+   * @returns {$data.DataContainer}
+   */
+  getSilentNodeWrapped: function () {
+    return $data.DataContainer.create({data: this.getSilentNode()});
+  },
+
+  /**
+   * Checks entity node and triggers appropriate events, but doesn't return
+   * the value.
+   * @returns {$entity.Entity}
+   */
+  touchNode: function () {
+    this.getNode();
+    return this;
   }
+});
+
+// todo Might need to move these to globals.js
+$oop.copyProperties($entity, /** @lends $entity */{
+  /**
+   * Signals a failed attempt to access the entity in the entity store.
+   * (Node was not there.)
+   * @constant
+   */
+  EVENT_ENTITY_ABSENT: 'EVENT_ENTITY_ABSENT'
 });
--- modules/cake-entity/entities/Entity.spec.js
+++ modules/cake-entity/entities/Entity.spec.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var $oop = window['cake-oop'],
+    $data = window['cake-data'],
     $entity = window['cake-entity'];
 
 describe("$entity", function () {
@@ -13,14 +14,11 @@ describe("$entity", function () {
     beforeEach(function () {
       Entity = $oop.getClass('test.$entity.Entity.Entity')
       .mix($entity.Entity);
+      entityKey = 'foo/bar'.toDocumentKey();
+      entity = Entity.fromEntityKey(entityKey);
     });
 
     describe("fromEntityKey()", function () {
-      beforeEach(function () {
-        entityKey = 'foo/bar'.toDocumentKey();
-        entity = Entity.fromEntityKey(entityKey);
-      });
-
       it("should return an Entity instance", function () {
         expect(Entity.mixedBy(entity)).toBeTruthy();
       });
@@ -31,18 +29,121 @@ describe("$entity", function () {
     });
 
     describe("getNode()", function () {
+      var node;
+
+      beforeEach(function () {
+        node = {};
+        $entity.entities.setNode('document.foo.bar'.toPath(), node);
+        result = entity.getNode();
+      });
+
+      afterEach(function () {
+        $entity.entities.deletePath('document.foo.bar'.toPath());
+      });
+
+      it("should return entity node", function () {
+        expect(result).toBe(node);
+      });
+
+      describe("when entity node is absent", function () {
+        beforeEach(function () {
+          spyOn(entity, 'trigger');
+          $entity.entities.deleteNode('document.foo.bar'.toPath());
+          result = entity.getNode();
+        });
+
+        it("should return undefined", function () {
+          expect(result).toBeUndefined();
+        });
+
+        it("should trigger event", function () {
+          expect(entity.trigger).toHaveBeenCalledWith('EVENT_ENTITY_ABSENT');
+        });
+      });
     });
 
     describe("getNodeWrapped()", function () {
+      var node;
+
+      beforeEach(function () {
+        node = {};
+        spyOn(entity, 'getNode').and.returnValue(node);
+        result = entity.getNodeWrapped();
+      });
+
+      it("should return a DataContainer instance", function () {
+        expect($data.DataContainer.mixedBy(result)).toBeTruthy();
+      });
+
+      it("should wrap node in DataContainer", function () {
+        expect(result.data).toBe(node);
+      });
     });
 
     describe("getSilentNode()", function () {
+      var node;
+
+      beforeEach(function () {
+        node = {};
+        $entity.entities.setNode('document.foo.bar'.toPath(), node);
+        result = entity.getSilentNode();
+      });
+
+      afterEach(function () {
+        $entity.entities.deletePath('document.foo.bar'.toPath());
+      });
+
+      it("should return entity node", function () {
+        expect(result).toBe(node);
+      });
     });
 
     describe("getSilentNodeWrapped()", function () {
+      var node;
+
+      beforeEach(function () {
+        node = {};
+        spyOn(entity, 'getSilentNode').and.returnValue(node);
+        result = entity.getSilentNodeWrapped();
+      });
+
+      it("should return a DataContainer instance", function () {
+        expect($data.DataContainer.mixedBy(result)).toBeTruthy();
+      });
+
+      it("should wrap node in DataContainer", function () {
+        expect(result.data).toBe(node);
+      });
     });
 
     describe("touchNode()", function () {
+      var node;
+
+      beforeEach(function () {
+        node = {};
+        $entity.entities.setNode('document.foo.bar'.toPath(), node);
+        result = entity.touchNode();
+      });
+
+      afterEach(function () {
+        $entity.entities.deletePath('document.foo.bar'.toPath());
+      });
+
+      it("should return self", function () {
+        expect(result).toBe(entity);
+      });
+
+      describe("when entity node is absent", function () {
+        beforeEach(function () {
+          spyOn(entity, 'trigger');
+          $entity.entities.deleteNode('document.foo.bar'.toPath());
+          result = entity.getNode();
+        });
+
+        it("should trigger event", function () {
+          expect(entity.trigger).toHaveBeenCalledWith('EVENT_ENTITY_ABSENT');
+        });
+      });
     });
 
     describe("setNode()", function () {
