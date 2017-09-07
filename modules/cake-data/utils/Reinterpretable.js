56e6f583864bfaf3533679f1dc35dda4d5b4c4d8
Dan Stocker <dan@kwaia.com>
2017-09-07 13:45:37 +0100

    Removes TODOs

--- modules/cake-data/utils/Reinterpretable.js
+++ modules/cake-data/utils/Reinterpretable.js
@@ -4,7 +4,6 @@
  * Describes a class the instances of which may be reinterpreted to other
  * classes.
  * @interface $data.Reinterpretable
- * @todo Move to cake-utils?
  */
 $data.Reinterpretable = $oop.getClass('$data.Reinterpretable')
 .define(/** @lends $data.Reinterpretable# */{
--- modules/cake-data/utils/Transformable.js
+++ modules/cake-data/utils/Transformable.js
@@ -4,7 +4,6 @@
  * Describes a class the instances of which may be transformed into
  * instances of other classes.
  * @interface $data.Transformable
- * @todo Move to cake-utils?
  */
 $data.Transformable = $oop.getClass('$data.Transformable')
 .define(/** @lends $data.Transformable# */{
