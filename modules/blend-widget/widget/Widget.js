4e045032ca5bbddea77ce79e1afb99be2ead9794
Dan Stocker <dan@kwaia.com>
2017-10-24 10:21:04 +0100

    Adds attach & detach handlers to $widget.Widget

--- modules/blend-widget/widget/Widget.js
+++ modules/blend-widget/widget/Widget.js
@@ -2,7 +2,7 @@
 
 /**
  * @function $widget.Widget.create
- * @param {Object} properties
+ * @param {Object} [properties]
  * @param {string} [properties.nodeName]
  * @param {string} [properties.subscriberId]
  * @returns {$widget.Widget}
@@ -65,5 +65,64 @@ $widget.Widget = $oop.getClass('$widget.Widget')
     .forEach(function (classId) {
       that.addCssClass(classId);
     });
+  },
+
+  /**
+   * @param {$widget.Widget} node
+   * @returns {$widget.Widget}
+   */
+  addChildNode: function addChildNode(node) {
+    var childNodeBefore = addChildNode.saved.childNodeBefore;
+    if (childNodeBefore !== node && this.isAttached()) {
+      node.onAttach();
+    }
+    return this;
+  },
+
+  /**
+   * @param {string} childNodeName
+   * @returns {$widget.Widget}
+   */
+  removeChildNode: function removeChildNode(childNodeName) {
+    var childNodeBefore = removeChildNode.saved.childNodeBefore;
+    if (childNodeBefore && this.isAttached()) {
+      childNodeBefore.onDetach();
+    }
+    return this;
+  },
+
+  /**
+   * @returns {boolean}
+   */
+  isAttached: function () {
+    var parentWidget = this;
+    while (parentWidget.parentNode) {
+      parentWidget = parentWidget.parentNode;
+    }
+    return parentWidget === $widget.RootWidget.create();
+  },
+
+  /** @ignore */
+  onAttach: function () {
+    this.childNodeLookup.callOnEachValue('onAttach');
+  },
+
+  /** @ignore */
+  onDetach: function () {
+    this.childNodeLookup.callOnEachValue('onDetach');
+  }
+});
+
+$oop.copyProperties(String.prototype, /** @lends String# */{
+  /**
+   * @returns {$widget.Widget}
+   */
+  toWidget: function () {
+    var elementId = this.valueOf(),
+        instanceId = parseInt(elementId.substr(1), 10),
+        widget = $widget.Widget.getInstanceById(instanceId);
+    return $widget.Widget.mixedBy(widget) ?
+        widget :
+        undefined;
   }
 });
--- modules/blend-widget/widget/Widget.spec.js
+++ modules/blend-widget/widget/Widget.spec.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var $oop = window['blend-oop'],
+    $utils = window['blend-utils'],
     $widget = window['blend-widget'];
 
 describe("$widget", function () {
@@ -44,6 +45,191 @@ describe("$widget", function () {
           '$widget.Widget': 1
         }));
       });
+    });
+
+    describe("addChildNode()", function () {
+      var childWidget1,
+          childWidget2;
+
+      beforeEach(function () {
+        widget = Widget.fromNodeName('foo');
+        childWidget1 = Widget.fromNodeName('bar');
+        childWidget2 = Widget.fromNodeName('baz');
+        childWidget1.addChildNode(childWidget2);
+      });
+
+      it("should return self", function () {
+        var result = widget.addChildNode(childWidget1);
+        expect(result).toBe(widget);
+      });
+
+      describe("when parent is attached", function () {
+        beforeEach(function () {
+          widget.addToParentNode($widget.RootWidget.create());
+        });
+
+        describe("when passing different node", function () {
+          beforeEach(function () {
+            spyOn(childWidget1, 'onAttach').and.callThrough();
+            spyOn(childWidget2, 'onAttach').and.callThrough();
+          });
+
+          it("should invoke onAttach on new children", function () {
+            widget.addChildNode(childWidget1);
+            expect(childWidget1.onAttach).toHaveBeenCalled();
+            expect(childWidget2.onAttach).toHaveBeenCalled();
+          });
+        });
+
+        describe("when passing same node", function () {
+          beforeEach(function () {
+            widget.addChildNode(childWidget1);
+            spyOn(childWidget1, 'onAttach').and.callThrough();
+            spyOn(childWidget2, 'onAttach').and.callThrough();
+          });
+
+          it("should not invoke onAttach on new children", function () {
+            widget.addChildNode(childWidget1);
+            expect(childWidget1.onAttach).not.toHaveBeenCalled();
+            expect(childWidget2.onAttach).not.toHaveBeenCalled();
+          });
+        });
+      });
+
+      describe("when parent is detached", function () {
+        describe("when passing different node", function () {
+          beforeEach(function () {
+            spyOn(childWidget1, 'onAttach').and.callThrough();
+            spyOn(childWidget2, 'onAttach').and.callThrough();
+          });
+
+          it("should not invoke onAttach on new children", function () {
+            widget.addChildNode(childWidget1);
+            expect(childWidget1.onAttach).not.toHaveBeenCalled();
+            expect(childWidget2.onAttach).not.toHaveBeenCalled();
+          });
+        });
+      });
+    });
+
+    describe("removeChildNode()", function () {
+      var childWidget1,
+          childWidget2;
+
+      beforeEach(function () {
+        widget = Widget.fromNodeName('foo');
+        childWidget1 = Widget.fromNodeName('bar');
+        childWidget2 = Widget.fromNodeName('baz');
+        childWidget1.addChildNode(childWidget2);
+      });
+
+      it("should return self", function () {
+        var result = widget.removeChildNode(childWidget1);
+        expect(result).toBe(widget);
+      });
+
+      describe("when parent is attached", function () {
+        beforeEach(function () {
+          widget.addToParentNode($widget.RootWidget.create());
+        });
+
+        describe("when passing existing child node", function () {
+          beforeEach(function () {
+            widget.addChildNode(childWidget1);
+            spyOn(childWidget1, 'onDetach').and.callThrough();
+            spyOn(childWidget2, 'onDetach').and.callThrough();
+          });
+
+          it("should invoke onDetach", function () {
+            widget.removeChildNode('bar');
+            expect(childWidget1.onDetach).toHaveBeenCalled();
+            expect(childWidget2.onDetach).toHaveBeenCalled();
+          });
+        });
+
+        describe("when passing absent child node", function () {
+          it("should not invoke onDetach", function () {
+            beforeEach(function () {
+              spyOn(childWidget1, 'onDetach').and.callThrough();
+              spyOn(childWidget2, 'onDetach').and.callThrough();
+            });
+
+            it("should not invoke onDetach", function () {
+              widget.removeChildNode('bar');
+              expect(childWidget1.onDetach).not.toHaveBeenCalled();
+              expect(childWidget2.onDetach).not.toHaveBeenCalled();
+            });
+          });
+        });
+      });
+
+      describe("when parent is detached", function () {
+        describe("when passing existing child node", function () {
+          beforeEach(function () {
+            widget.addChildNode(childWidget1);
+            spyOn(childWidget1, 'onDetach').and.callThrough();
+            spyOn(childWidget2, 'onDetach').and.callThrough();
+          });
+
+          it("should not invoke onDetach", function () {
+            widget.removeChildNode('bar');
+            expect(childWidget1.onDetach).not.toHaveBeenCalled();
+            expect(childWidget2.onDetach).not.toHaveBeenCalled();
+          });
+        });
+      });
+    });
+
+    describe("isAttached()", function () {
+      beforeEach(function () {
+        widget = Widget.create();
+      });
+
+      describe("when node descends from RootWidget", function () {
+        beforeEach(function () {
+          widget
+          .addToParentNode(
+              Widget.create()
+              .addToParentNode(
+                  $widget.RootWidget.create()));
+        });
+
+        it("should return truthy", function () {
+          expect(widget.isAttached()).toBeTruthy();
+        });
+      });
+
+      describe("when node does not descend from RootWidget", function () {
+        it("should return falsy", function () {
+          expect(widget.isAttached()).toBeFalsy();
+        });
+      });
+    });
+  });
+});
+
+describe("String", function () {
+  describe("toWidget()", function () {
+    var instances,
+        lastInstanceId;
+
+    beforeEach(function () {
+      lastInstanceId = $utils.Identifiable.lastInstanceId;
+      instances = $utils.Retrievable.instances;
+      $utils.Retrievable.instances = {};
+      $utils.Identifiable.lastInstanceId = -1;
+
+      $widget.Widget.create();
+    });
+
+    afterEach(function () {
+      $utils.Retrievable.instances = instances;
+      $utils.Identifiable.lastInstanceId = lastInstanceId;
+    });
+
+    it("should return matching widget from registry", function () {
+      expect('w0'.toWidget()).toBe($utils.Retrievable.instances[0]);
+      expect('w1'.toWidget()).toBeUndefined();
     });
   });
 });
