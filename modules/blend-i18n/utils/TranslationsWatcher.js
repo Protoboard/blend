599d111c81eea57aed9d9b8d2b611213cee7f7ca
Dan Stocker <dan@kwaia.com>
2017-10-30 12:34:21 +0000

    Adds $i18n.TranslationsWatcher

---
+++ modules/blend-i18n/utils/TranslationsWatcher.js
@@ -0,0 +1,52 @@
+"use strict";
+
+/**
+ * @function $i18n.TranslationsWatcher.create
+ * @returns {$i18n.TranslationsWatcher}
+ */
+
+/**
+ * @class $i18n.TranslationsWatcher
+ * @extends $event.EventSender
+ * @extends $event.EventListener
+ * @extends $event.EventSubscriber
+ * @implements $utils.Destructible
+ */
+$i18n.TranslationsWatcher = $oop.getClass('$i18n.TranslationsWatcher')
+.blend($oop.Singleton)
+.blend($event.EventSender)
+.blend($event.EventListener)
+.blend($event.EventSubscriber)
+.implement($utils.Destructible)
+.define(/** @lends $i18n.TranslationsWatcher# */{
+  /** @ignore */
+  spread: function () {
+    this.subscriberId = this.__classId;
+    this.listeningPath = 'entity.document.__field._locale/translations';
+  },
+
+  /** @ignore */
+  init: function () {
+    this.on($entity.EVENT_ENTITY_CHANGE, this, this.onTranslationsChange);
+  },
+
+  /**
+   * @param {$entity.EntityChangeEvent} event
+   * @ignore
+   */
+  onTranslationsChange: function (event) {
+    var translationsField = event.sender,
+        localeDocumentKey = translationsField.entityKey.documentKey;
+
+    return localeDocumentKey.toDocument()
+    .trigger($i18n.EVENT_TRANSLATIONS_CHANGE);
+  }
+});
+
+$oop.copyProperties($i18n, /** @lends $i18n */{
+  /**
+   * Signals that translations associated with a locale have changed.
+   * @constant
+   */
+  EVENT_TRANSLATIONS_CHANGE: 'i18n.change.translations'
+});
---
+++ modules/blend-i18n/utils/TranslationsWatcher.spec.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var $oop = window['blend-oop'],
+    $i18n = window['blend-i18n'];
+
+describe("$i18n", function () {
+  describe("TranslationsWatcher", function () {
+    var TranslationsWatcher,
+        translationsWatcher;
+
+    beforeAll(function () {
+      TranslationsWatcher = $oop.getClass('test.$i18n.TranslationsWatcher.TranslationsWatcher')
+      .blend($i18n.TranslationsWatcher);
+      TranslationsWatcher.__forwards = {list: [], sources: [], lookup: {}};
+    });
+
+    it("should be singleton", function () {
+      expect(TranslationsWatcher.create()).toBe(TranslationsWatcher.create());
+    });
+
+    describe("create()", function () {
+      it("should initialize subscriberId", function () {
+        translationsWatcher = TranslationsWatcher.create();
+        expect(translationsWatcher.subscriberId)
+        .toBe('test.$i18n.TranslationsWatcher.TranslationsWatcher');
+      });
+
+      it("should initialize listeningPath", function () {
+        translationsWatcher = TranslationsWatcher.create();
+        expect(translationsWatcher.listeningPath)
+        .toBe('entity.document.__field._locale/translations');
+      });
+    });
+
+    describe("onTranslationsChange()", function () {
+      beforeEach(function () {
+        translationsWatcher = TranslationsWatcher.create();
+        '_translation/foo-en-us'.toDocument().deleteNode();
+        '_locale/en-us/translations'.toField().deleteNode();
+        spyOn($i18n.LocaleDocument, 'trigger');
+      });
+
+      it("should trigger EVENT_TRANSLATIONS_CHANGE on locale", function () {
+        '_translation/foo-en-us'.toDocument().setNode({
+          locale: '_locale/en-us',
+          originalString: 'foo',
+          pluralForms: ['foo', 'foo']
+        });
+        '_locale/en-us/translations'.toField().appendNode({
+          '_translation/foo-en-us': 1,
+          '_translation/foo-de': 1
+        });
+
+        var calls = $i18n.LocaleDocument.trigger.calls.all();
+        expect(calls.length).toBe(1);
+        expect(calls[0].object).toEqual('_locale/en-us'.toDocument());
+        expect(calls[0].args).toEqual(['i18n.change.translations']);
+      });
+    });
+  });
+});
