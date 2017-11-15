d440d7cb1b5edb0e41613381d6c6ba48f740f959
Dan Stocker <dan@kwaia.com>
2017-11-15 10:42:13 +0000

    Fixes annotations in $widgets.DomLocaleText

--- modules/blend-widgets/browser/DomLocaleText.js
+++ modules/blend-widgets/browser/DomLocaleText.js
@@ -2,12 +2,16 @@
 
 /**
  * @mixin $widgets.DomLocaleText
+ * @extends $widgets.DomText
+ * @augments $widgets.LocaleText
+ * @augments $widgets.XmlText
  */
 $widgets.DomLocaleText = $oop.getClass('$widgets.DomLocaleText')
 .blend($oop.getClass('$widgets.DomText'))
 .expect($oop.getClass('$widgets.LocaleText'))
 .expect($oop.getClass('$widgets.XmlText'))
 .define(/** @lends $widgets.DomLocaleText# */{
+  /** @ignore */
   syncToActiveLocale: function () {
     // `Translatable` content will render (stringify) to translation in
     // active locale
