8626e7d73efe4b2d35698caf441072b8305a24c5
Dan Stocker <dan@kwaia.com>
2017-11-17 08:55:24 +0000

    Refactors $widgets.LocaleText to sync insteadof reRender

--- modules/blend-widgets/text/LocaleText.js
+++ modules/blend-widgets/text/LocaleText.js
@@ -12,4 +12,31 @@
  */
 $widgets.LocaleText = $oop.getClass('$widgets.LocaleText')
 .blend($oop.getClass('$widgets.Text'))
-.blend($oop.getClass('$widgets.LocaleBound'));
+.blend($oop.getClass('$widgets.LocaleBound'))
+.define(/** @lends $widgets.LocaleText# */{
+  /**
+   * @member {$i18n.Translatable} $widgets.LocaleText#textTranslatable
+   */
+
+  /**
+   * @memberOf $widgets.LocaleText
+   * @param textTranslatable
+   * @returns {$widgets.LocaleText}
+   */
+  fromTextTranslatable: function (textTranslatable) {
+    return this.create({
+      textTranslatable: textTranslatable
+    });
+  },
+
+  /** @ignore */
+  init: function () {
+    $assert.isInstanceOf(
+        this.textTranslatable, $i18n.Translatable, "Invalid textTranslatable");
+  },
+
+  /** @ignore */
+  syncToActiveLocale: function () {
+    this.setTextString(this.textTranslatable.toString());
+  }
+});
