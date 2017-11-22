fb213434c5d6613ad06c9978944120b125a2cce9
Dan Stocker <dan@kwaia.com>
2017-11-22 17:54:29 +0000

    Fixes dependencies of $ui.HtmlText

--- modules/blend-ui/html/HtmlText.js
+++ modules/blend-ui/html/HtmlText.js
@@ -4,10 +4,12 @@
  * @mixin $ui.HtmlText
  * @extends $widget.HtmlWidget
  * @extends $ui.TextContentHost
+ * @augments $ui.Text
  */
 $ui.HtmlText = $oop.getClass('$ui.HtmlText')
 .blend($widget.HtmlWidget)
 .blend($oop.getClass('$ui.TextContentHost'))
+.expect($oop.getClass('$ui.Text'))
 .define(/** @lends $ui.HtmlText# */{
   /** @ignore */
   defaults: function () {
