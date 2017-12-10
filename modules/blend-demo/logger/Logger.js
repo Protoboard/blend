f509847ab6beec7c6b6081f145d3b22ea1c7b5a9
Dan Stocker <dan@kwaia.com>
2017-12-10 11:57:04 +0000

    Adds $demo.Logger

---
+++ modules/blend-demo/logger/Logger.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/**
+ * @class $demo.Logger
+ */
+$demo.Logger = $oop.createClass('$demo.Logger')
+.build();
+
+$event.EventSpace.create()
+.on($router.EVENT_ROUTE_CHANGE,
+    'route',
+    $demo.Logger.__className,
+    function (event) {
+      console.log("route changed from `" + event.routeBefore + "` to `" + event.routeAfter + "`");
+    })
+.on($entity.EVENT_ENTITY_CHANGE,
+    'entity',
+    $demo.Logger.__className,
+    function (event) {
+      console.log(
+          "entity `" + event.sender.entityKey + "` changed" +
+          (event.nodeBefore !== event.nodeAfter ?
+              " from `" + event.nodeBefore + "` to `" + event.nodeAfter + "`" :
+              " adding `" + event.propertiesAdded + "`, removing `" + event.propertiesRemoved + "`"), event);
+    })
+.on($widget.EVENT_STATE_CHANGE,
+    'widget',
+    $demo.Logger.__className,
+    function (event) {
+      console.log("widget `" + event.sender.instanceId + "` state `" +
+          event.stateName + "` changed from `" + event.stateValueBefore +
+          "` to  `" + event.stateValueAfter + "`");
+    })
+.on($widget.EVENT_CLICKABLE_CLICK,
+    'widget',
+    $demo.Logger.__className,
+    function (event) {
+      console.log("widget `" + event.sender.instanceId + "` clicked");
+    })
+.on($i18n.EVENT_LOCALE_CHANGE,
+    'locale',
+    $demo.Logger.__className,
+    function (event) {
+      console.log("locale changed from `" + event.localeBefore + "` to  `" +
+          event.localeAfter + "`");
+    });
